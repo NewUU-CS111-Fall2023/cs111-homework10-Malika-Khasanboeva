78bac9b4f556ec57e502bf7ed3b3a19c46716024
Malika-Khasanboeva <127714681+Malika-Khasanboeva@users.noreply.github.com>
2024-01-19 23:58:02 +0500

    updated

--- src/cpp/task_3.cpp
+++ src/cpp/task_3.cpp
@@ -1,8 +1,29 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+using namespace std;
 
-class Problem3 {
-};
+int main() {
+    string num1, num2;
+    cin >> num1 >> num2;
+    int i = 0, j = 0;
+    while (num1[i] == '0') i++; // skip leading zeros
+    while (num2[j] == '0') j++;
+    if (num1.size() - i > num2.size() - j) {
+        cout << 1 << endl;
+    } else if (num1.size() - i < num2.size() - j) {
+        cout << 1 << " " << 1 << endl;
+    } else {
+        while (i < num1.size()) {
+            if (num1[i] > num2[j]) {
+                cout << 1 << endl;
+                return 0;
+            } else if (num2[j] > num1[i]) {
+                cout << 1 << " " << 1 << endl;
+                return 0;
+            }
+            i++;
+            j++;
+        }
+        cout << 1 << endl; // numbers are equal
+    }
+    return 0;
+}
